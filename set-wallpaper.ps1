23ce6497597152418deba0c5c753cf485c34e3a3
Felipegf-hub <filipeocara2@gmail.com>
2026-08-24 08:46:26 -0300

    Add files via upload

--- set-wallpaper.ps1
+++ set-wallpaper.ps1
@@ -1,19 +1,27 @@
 $url = "https://raw.githubusercontent.com/Felipegf-hub/wallpaper-empresa/main/desktop_NPE.jpg"
 $path = "$env:APPDATA\wallpaper.jpg"
 $hashFile = "$env:APPDATA\wallpaper.hash"
+$logFile = "$env:APPDATA\wallpaper.log"
 
 try {
+    "$(Get-Date) - Iniciando..." | Out-File $logFile -Append
+
     $tempPath = "$env:TEMP\wallpaper_novo.jpg"
     Invoke-WebRequest -Uri $url -OutFile $tempPath -UseBasicParsing
 
     $novoHash = (Get-FileHash $tempPath -Algorithm SHA256).Hash
     $hashAntigo = if (Test-Path $hashFile) { Get-Content $hashFile } else { "" }
 
-    if ($novoHash -ne $hashAntigo) {
-        Copy-Item $tempPath $path -Force
-        Set-Content -Path $hashFile -Value $novoHash
+    "$(Get-Date) - Hash novo: $novoHash | Hash antigo: $hashAntigo" | Out-File $logFile -Append
 
-        Add-Type -TypeDefinition @"
+    Copy-Item $tempPath $path -Force
+    Set-Content -Path $hashFile -Value $novoHash
+
+    # Garante o estilo "Preencher" (evita imagem nao aparecer corretamente)
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -Value "10"
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -Value "0"
+
+    Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 public class Wallpaper {
@@ -21,10 +29,10 @@ public class Wallpaper {
     public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 }
 "@
-        [Wallpaper]::SystemParametersInfo(20, 0, $path, 3)
-    }
+    $resultado = [Wallpaper]::SystemParametersInfo(20, 0, $path, 3)
+    "$(Get-Date) - Resultado SystemParametersInfo: $resultado" | Out-File $logFile -Append
 
     Remove-Item $tempPath -Force -ErrorAction SilentlyContinue
 } catch {
-    Write-Output "Erro ao atualizar wallpaper: $_"
+    "$(Get-Date) - ERRO: $_" | Out-File $logFile -Append
 }
